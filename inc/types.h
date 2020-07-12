4ff7868ec043fc9eb3efcc49e6b1118dd5ade04a
Nala Ginrut <mulei@gnu.org>
2020-07-13 04:45:54 +0800

    Fix types.h

--- inc/types.h
+++ inc/types.h
@@ -25,13 +25,8 @@
 #include <stddef.h>
 #define bool _Bool
 
-#elif defined LAMBDACHIP_LINUX
-#define CONFIG_HEAP_MEM_POOL_SIZE 4096
-#include <types.h>
-#include <stddef.h>
-#include <stdbool.h>
-
 #else
+#define CONFIG_HEAP_MEM_POOL_SIZE 90000
 #include "__types.h"
 #endif
 
