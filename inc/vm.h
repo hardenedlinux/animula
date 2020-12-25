eea60dff26fbbe156eb2fc447aca33d1e7386ec9
Nala Ginrut <mulei@gnu.org>
2020-12-25 15:39:46 +0800

    Fix to call RESTORE juse once

--- inc/vm.h
+++ inc/vm.h
@@ -328,17 +328,18 @@ static inline void vm_stack_check (vm_t vm)
  * So we pop twice to skip its own prelude to restore the last
  * frame.
  */
-#define RESTORE()               \
-  for (int i = 0; i < 2; i++)   \
-    {                           \
-      vm->sp = vm->fp + FPS;    \
-      vm->fp = POP_REG ();      \
-      vm->pc = POP_REG ();      \
-      vm->local = vm->fp + FPS; \
-      if (vm->closure)          \
-        vm->closure = NULL;     \
-    }                           \
-  while (0)
+#define RESTORE()                 \
+  do                              \
+    {                             \
+      {                           \
+        vm->sp = vm->fp + FPS;    \
+        vm->fp = POP_REG ();      \
+        vm->pc = POP_REG ();      \
+        vm->local = vm->fp + FPS; \
+        if (vm->closure)          \
+          vm->closure = NULL;     \
+      }                           \
+      while (0)
 
 #define CALL_PROCEDURE(obj)           \
   do                                  \
