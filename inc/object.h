d9f851d02586939632f9913626a9266d768b1af6
Nala Ginrut <mulei@gnu.org>
2025-06-02 20:40:49 +0900

    Add customizable MAX_STR_LEN

--- inc/object.h
+++ inc/object.h
@@ -1,6 +1,6 @@
 #ifndef __ANIMULA_OBJECT_H__
 #define __ANIMULA_OBJECT_H__
-/*  Copyright (C) 2020-2021
+/*  Copyright (C) 2020-2025
  *        "Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
  *  Animula is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as
@@ -25,49 +25,55 @@
 
 /*
  * NOTE:
- * Mostly, LambdaChip supports 32bit MCU, so the object uses 32bit encoding.
+ * Mostly, Animula supports 32bit MCU, so the object uses 32bit encoding.
 
-  Type:             0                     15         23           31
-  |                 |                      |                      |
-  0.  Imm Int       |              32bit signed int               |
-  1.  Arbi Int      |      next cell       |      16bit int       |
-  2.  Keyword       |      head pointer                           |
-  3.  Pair          |      pair_t address                         |
-  4.  Symbol        |      interned head pointer                  |
-  5.  Vector        |      length          |      content         |
-  6.  Continuation  |      parent          |      closure         |
-  7.  List          |      list_t address                         |
-  8.  String        |      C-string encoding                      |
-  9.  Procedure     |      entry           |  arity  |   opt      |
-  10. Primitive     |      primitive number                       |
+ Type:             0                     15         23           31
+ |                 |                      |                      |
+ 0.  Imm Int       |              32bit signed int               |
+ 1.  Arbi Int      |      next cell       |      16bit int       |
+ 2.  Keyword       |      head pointer                           |
+ 3.  Pair          |      pair_t address                         |
+ 4.  Symbol        |      interned head pointer                  |
+ 5.  Vector        |      length          |      content         |
+ 6.  Continuation  |      parent          |      closure         |
+ 7.  List          |      list_t address                         |
+ 8.  String        |      C-string encoding                      |
+ 9.  Procedure     |      entry           |  arity  |   opt      |
+ 10. Primitive     |      primitive number                       |
 
-  Closure on heap
-                    0                                            31
-  11. Closure       |      closure_t address                      |
+ Closure on heap
+ 0                                            31
+ 11. Closure       |      closure_t address                      |
 
-  Closure on stack
-                    0              9      15                     31
-  12. Closure       |      env     | size  |      entry offset    |
+ Closure on stack
+ 0              9      15                     31
+ 12. Closure       |      env     | size  |      entry offset    |
 
-  13. Real          |      IEEE 754                               |
+ 13. Real          |      IEEE 754                               |
 
-  NOTE: The rational number is canonical-form handled by the compiler
-  NOTE: We seperate +/- rational number to save RAMs
-  14. +Rational     |      16bit uint      |       16bit uint     |
-  15. -Rational     |      16bit uint      |       16bit uint     |
+ NOTE: The rational number is canonical-form handled by the compiler
+ NOTE: We seperate +/- rational number to save RAMs
+ 14. +Rational     |      16bit uint      |       16bit uint     |
+ 15. -Rational     |      16bit uint      |       16bit uint     |
 
-  16. Complex       |      16bit sint      |       16bit sint     |
-  17. Complex       |      inexact complex address                |
+ 16. Complex       |      16bit sint      |       16bit sint     |
+ 17. Complex       |      inexact complex address                |
 
-  18. Mut string    |      malloced C-string header               |
-  18. Mut list      |      list_t address                         |
+ 18. Mut string    |      malloced C-string header               |
+ 18. Mut list      |      list_t address                         |
 
-  61. Boolean       |      false: 0,     true: 1                  |
-  62. null_object   |                                             |
-  63. None          |      const encoding                         |
+ 61. Boolean       |      false: 0,     true: 1                  |
+ 62. null_object   |                                             |
+ 63. None          |      const encoding                         |
 */
 
-#define MAX_STR_LEN 256
+/*
+ * Builders may define their preferred maximum string length in compiling.
+ * -D MAX_STR_LEN=1024
+ */
+#ifndef MAX_STR_LEN
+#  define MAX_STR_LEN 256
+#endif
 
 extern GLOBAL_DEF (const Object, true_const);
 extern GLOBAL_DEF (const Object, false_const);
@@ -134,68 +140,68 @@ static inline bool is_unspecified (object_t obj)
     x;                                          \
   })
 
-#define NEW_LIST_NODE()                \
-  ({                                   \
-    list_node_t ol = NULL;             \
-    do                                 \
-      {                                \
-        ol = animula_new_list_node (); \
-        if (ol)                        \
-          {                            \
-            break;                     \
-          }                            \
-        GC ();                         \
-      }                                \
-    while (1);                         \
-    ol;                                \
+#define NEW_LIST_NODE()				\
+  ({						\
+    list_node_t ol = NULL;			\
+    do						\
+      {						\
+        ol = animula_new_list_node ();		\
+        if (ol)					\
+          {					\
+            break;				\
+          }					\
+        GC ();					\
+      }						\
+    while (1);					\
+    ol;						\
   })
 
-#define CREATE_NEW_OBJ(t, te, to)         \
-  do                                      \
-    {                                     \
-      t o = (t)gc_pool_malloc (te);       \
-      if (!o)                             \
-        {                                 \
-          o = (t)GC_MALLOC (sizeof (to)); \
-        }                                 \
-      return o;                           \
-    }                                     \
+#define CREATE_NEW_OBJ(t, te, to)		\
+  do						\
+    {						\
+      t o = (t)gc_pool_malloc (te);		\
+      if (!o)					\
+        {					\
+          o = (t)GC_MALLOC (sizeof (to));	\
+        }					\
+      return o;					\
+    }						\
   while (0)
 
-#define VALIDATE(obj, t)                                      \
-  do                                                          \
-    {                                                         \
-      if ((t) != (obj)->attr.type)                            \
-        {                                                     \
-          PANIC ("Invalid type, expect %d, but it's %d\n", t, \
-                 (obj)->attr.type);                           \
-        }                                                     \
-    }                                                         \
+#define VALIDATE(obj, t)					\
+  do								\
+    {								\
+      if ((t) != (obj)->attr.type)				\
+        {							\
+          PANIC ("Invalid type, expect %d, but it's %d\n", t,	\
+                 (obj)->attr.type);				\
+        }							\
+    }								\
   while (0)
 
 #define CHAR_VALUE_VALID(c) ((0 < c && c <= MAX_UINT8) ? true : false)
 
-#define VALIDATE_STRING(obj)                                                \
-  do                                                                        \
-    {                                                                       \
+#define VALIDATE_STRING(obj)						\
+  do									\
+    {									\
       if ((string != (obj)->attr.type) && (mut_string != (obj)->attr.type)) \
-        {                                                                   \
-          PANIC ("Invalid type, expect string, but it's %d\n",              \
-                 (obj)->attr.type);                                         \
-        }                                                                   \
-    }                                                                       \
+        {								\
+          PANIC ("Invalid type, expect string, but it's %d\n",		\
+                 (obj)->attr.type);					\
+        }								\
+    }									\
   while (0)
 
-#define VALIDATE_BYTEVECTOR(obj)                                   \
-  do                                                               \
-    {                                                              \
-      if ((bytevector != (obj)->attr.type)                         \
-          && (mut_bytevector != (obj)->attr.type))                 \
-        {                                                          \
-          PANIC ("Invalid type, expect bytevector, but it's %d\n", \
-                 (obj)->attr.type);                                \
-        }                                                          \
-    }                                                              \
+#define VALIDATE_BYTEVECTOR(obj)					\
+  do									\
+    {									\
+      if ((bytevector != (obj)->attr.type)				\
+          && (mut_bytevector != (obj)->attr.type))			\
+        {								\
+          PANIC ("Invalid type, expect bytevector, but it's %d\n",	\
+                 (obj)->attr.type);					\
+        }								\
+    }									\
   while (0)
 
 #define MAX_REAL_DENOMINATOR 0xFFFF
@@ -211,40 +217,40 @@ static inline bool is_unspecified (object_t obj)
 #define MIN_UINT16           0
 #define MAX_UINT16           65535
 
-#define VALIDATE_NUMERATOR(x)                                             \
-  do                                                                      \
-    {                                                                     \
-      if (x < MIN_REAL_NUMERATOR || x > MAX_REAL_NUMERATOR)               \
-        {                                                                 \
+#define VALIDATE_NUMERATOR(x)						\
+  do									\
+    {									\
+      if (x < MIN_REAL_NUMERATOR || x > MAX_REAL_NUMERATOR)		\
+        {								\
           os_printk ("%s:%d, %s: NUMERATOR not in range: %d\n", __FILE__, \
-                     __LINE__, __FUNCTION__, x);                          \
-          panic ("");                                                     \
-        }                                                                 \
-    }                                                                     \
+                     __LINE__, __FUNCTION__, x);			\
+          panic ("");							\
+        }								\
+    }									\
   while (0)
 
-#define VALIDATE_DENOMINATOR(x)                                             \
-  do                                                                        \
-    {                                                                       \
-      if (x < MIN_REAL_DENOMINATOR || x > MAX_REAL_DENOMINATOR || x == 0)   \
-        {                                                                   \
+#define VALIDATE_DENOMINATOR(x)						\
+  do									\
+    {									\
+      if (x < MIN_REAL_DENOMINATOR || x > MAX_REAL_DENOMINATOR || x == 0) \
+        {								\
           os_printk ("%s:%d, %s: DENOMINATOR not in range: %d\n", __FILE__, \
-                     __LINE__, __FUNCTION__, x);                            \
-          panic ("");                                                       \
-        }                                                                   \
-    }                                                                       \
+                     __LINE__, __FUNCTION__, x);			\
+          panic ("");							\
+        }								\
+    }									\
   while (0)
 
-#define VALIDATE_NUMBER(obj)                                                  \
-  do                                                                          \
-    {                                                                         \
-      otype_t t = (obj)->attr.type;                                           \
+#define VALIDATE_NUMBER(obj)						\
+  do									\
+    {									\
+      otype_t t = (obj)->attr.type;					\
       if (imm_int != t && real != t && rational_pos != t && rational_neg != t \
-          && complex_exact != t && complex_inexact != t)                      \
-        {                                                                     \
-          PANIC ("Invalid type, expect numbers, but it's %d\n", t);           \
-        }                                                                     \
-    }                                                                         \
+          && complex_exact != t && complex_inexact != t)		\
+        {								\
+          PANIC ("Invalid type, expect numbers, but it's %d\n", t);	\
+        }								\
+    }									\
   while (0)
 
 #define OBJ_IS_ON_STACK(o) ((o)->attr.gc)
